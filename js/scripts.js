73eab1b414c1dfa03213aa44df0491214b969d7f
Kaz Matthews and Nicole Sanders <nicole@>
2017-01-30 09:41:40 -0800

    add click event listeners to buttons that show or hide furbies depending on their adoption status, and a click event listener to add an adopted class to adopted furbies

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ var furby1 = new Furby("Lola", 3, "gray, white");
 
 $(document).ready(function() {
   $("form").submit(function(event) {
+    event.preventDefault();
     var inputName = $("input#name").val();
     var inputAge = $("input#age").val();
     var inputColor = $("input#color").val();
@@ -16,4 +17,25 @@ $(document).ready(function() {
 
     $("span#furbyName").append(newFurby.furbyName, newFurby.age, newFurby.furbyColor, newFurby.adoptionStatus);
   });
+
+  $("button.adopt").click(function() {
+    $(this).parent().addClass("adopted");
+    $(this).parent().removeClass("notAdopted");
+  })
+
+  $("button.showAdopted").click(function() {
+    $(".adopted").show();
+    $(".notAdopted").hide();
+  })
+
+  $("button.showNotAdopted").click(function() {
+    $(".adopted").hide();
+    $(".notAdopted").show();
+  })
+
+  $("button.all").click(function() {
+    $(".adopted").show();
+    $(".notAdopted").show();
+  })
+
 });
